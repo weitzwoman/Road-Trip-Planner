8d737ad6e9c5835507d236492b873bd8df5c664f
haley swain and Tracie Weitzman <haley@>
2017-01-30 14:14:54 -0800

    implement waypoints in directions

--- src/app/map-main/map-main.component.ts
+++ src/app/map-main/map-main.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { GoogleMapsAPIWrapper } from 'angular2-google-maps/core';
+declare var google: any;
 
 @Component({
   selector: 'app-map-main',
   templateUrl: './map-main.component.html',
   styleUrls: ['./map-main.component.css']
 })
+
+// @Directive({
+//   selector: 'sebm-google-map-directions'
+// })
+
 export class MapMainComponent implements OnInit {
   title = 'Road Trip!';
-  lat: number = 45.798378;
-  lng: number = -108.630241;
+
+
+
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
