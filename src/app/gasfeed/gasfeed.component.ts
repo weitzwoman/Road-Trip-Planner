653f60c46ec03a46d6a0e6c68b6448328019efdb
Austin Blain and Anne Belka <anne@>
2017-01-30 16:32:35 -0800

    begin gasfeed api access

--- src/app/gasfeed/gasfeed.component.ts
+++ src/app/gasfeed/gasfeed.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { AppComponent } from '../app.component';
+import { MapService } from '../map.service';
 
 @Component({
   selector: 'app-gasfeed',
   templateUrl: './gasfeed.component.html',
-  styleUrls: ['./gasfeed.component.css']
+  styleUrls: ['./gasfeed.component.css'],
+  providers: [MapService]
 })
 export class GasfeedComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private mapService: MapService) { }
+   result = this.mapService.results;
   ngOnInit() {
+    console.log(this.result);
+  }
+
+  getMPG(lat, long) {
+    this.mapService.showMPG(lat, long);
   }
 
 }
--- src/app/map.service.ts
+++ src/app/map.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Map } from './map.model';
+import { Http, Response } from '@angular/http';
+import { masterGasApiConfig } from './api-keys';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class MapService {
+  results: any;
+  constructor(private http: Http) { }
 
-  constructor() { }
+  showMPG(lat, long) {
+    this.results = this.http.get("http://devapi.mygasfeed.com/stations/radius/45.677064/-122.551182/10/reg/price/rfej9napna.json?callback=?"
+);
+
+  }
 
 }
