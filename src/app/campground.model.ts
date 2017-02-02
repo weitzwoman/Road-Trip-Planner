62302b9772633e55cc05aeb1037c2b5a4ec2d71f
Anne Belka and haley swain <haley@>
2017-02-01 16:30:54 -0800

    add lat long to campground object

--- src/app/campground.model.ts
+++ src/app/campground.model.ts
@@ -1,3 +1,3 @@
 export class Campground {
-  constructor (public facilityName: string, public facilityDescription: string, public facilityDirections: string, public facilityTypeDescription: string) { }
+  constructor (public facilityName: string, public facilityDescription: string, public facilityDirections: string, public facilityTypeDescription: string, public facilityLatitude: string, public facilityLongitude: string) { }
 }
